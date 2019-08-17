325d6297caa5ae24d846ae8b9861ea6f02022c82
Alexandr Babich <avbabich@tut.by>
2019-08-17 10:36:08 +0300

    spec loader

--- app/workers/crawler_worker.rb
+++ app/workers/crawler_worker.rb
@@ -1,5 +1,6 @@
 require 'crawling/crawl'
 require 'loaders/estate_loader'
+require 'loaders/rate_loader'
 require 'data_storage.rb'
 
 class CrawlerWorker
@@ -8,6 +9,17 @@ class CrawlerWorker
 
   def perform(opts = {})
     Crawling::Crawl.new(opts.symbolize_keys).start
-    Loaders::EstateLoader.new(DataStorage.today_entity_path(opts['parser'])).load_data
+
+    parser = opts['parser']
+    loader = case parser
+             when /nbrb/
+               RateLoader
+             when /realt/
+               EstateLoader
+             else
+               raise "Unknown parser: #{parser.inspect}"
+             end
+
+    "Loaders::#{loader}".constantize.new(file: DataStorage.today_entity_path(parser)).load_data
   end
 end
